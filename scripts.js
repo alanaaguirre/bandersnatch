15c6d60dddc732a2e60c86a3af98ad8a294c8b2c
Alan Aguirre <alan.aaguirrej@gmail.com>
2024-01-05 09:44:52 -0600

    Corrección de scripts

--- scripts.js
+++ scripts.js
@@ -6,16 +6,27 @@ document.addEventListener("DOMContentLoaded", function () {
     progressAndFormContainer.id = "progressAndFormContainer";
     formContainer.appendChild(progressAndFormContainer);
 
+    const playButton = document.getElementById("playButton");
     const videoPath = "videos/home/main_clip.mp4";
     const progressBarDuration = 10;
 
-    videoPlayer.addEventListener("click", function () {
-        videoPlayer.src = videoPath;
-        videoPlayer.play();
-        resetProgressBar();
-    });
+    let isVideoPlaying = false;
+    let isDecisionPrompted = false;
+
+    playButton.addEventListener("click", playMainClip);
+
+    function playMainClip() {
+        if (!isVideoPlaying) {
+            videoPlayer.src = videoPath;
+            videoPlayer.play();
+            resetProgressBar();
+            isVideoPlaying = true;
+            playButton.style.display = "none"; // Ocultar el botón de reproducción al inicio
+        }
+    }
 
     videoPlayer.addEventListener("ended", function () {
+        isVideoPlaying = false;
         const currentSrc = videoPlayer.src;
         if (currentSrc.includes("_final")) {
             showFinalOptions();
@@ -28,28 +39,31 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     function showInitialOptions(currentSrc) {
-        let options;
+        if (!isDecisionPrompted) {
+            let options;
 
-        if (currentSrc.includes("h2_d2.mp4")) {
-            options = [
-                { video: "h2_d2_sec1_final.mp4", title: "Ir al cuarto 3" },
-                { video: "h2_d2_sec_final.mp4", title: "Ir al Rooftop" },
-            ];
-        } else if (currentSrc.includes("h2.mp4")) {
-            options = [
-                { video: "h2_d1_final.mp4", title: "Ponerse a trabajar" },
-                { video: "h2_d2.mp4", title: "Subir al 3er piso" },
-            ];
-        } else {
-            options = [
-                { video: "h1_final.mp4", title: "Cerrar las persianas" },
-                { video: "h2.mp4", title: "Sentarse en la silla" },
-            ];
+            if (currentSrc.includes("h2_d2.mp4")) {
+                options = [
+                    { video: "h2_d2_sec1_final.mp4", title: "Ir al cuarto 3" },
+                    { video: "h2_d2_sec2_final.mp4", title: "Ir al Rooftop" },
+                ];
+            } else if (currentSrc.includes("h2.mp4")) {
+                options = [
+                    { video: "h2_d1_final.mp4", title: "Ponerse a trabajar" },
+                    { video: "h2_d2.mp4", title: "Subir al 3er piso" },
+                ];
+            } else {
+                options = [
+                    { video: "h1_final.mp4", title: "Cerrar las persianas" },
+                    { video: "h2.mp4", title: "Sentarse en la silla" },
+                ];
+            }
+
+            const initialForm = createForm(options);
+            formContainer.innerHTML = "";
+            formContainer.appendChild(initialForm);
+            isDecisionPrompted = true;
         }
-
-        const initialForm = createForm(options);
-        formContainer.innerHTML = ""; // Limpiar contenido previo
-        formContainer.appendChild(initialForm);
     }
 
     function showFinalOptions() {
@@ -62,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
             finTitle.style.color = "white";
             finTitle.style.fontFamily = "sans-serif";
             finTitle.style.fontSize = "10rem";
-            formContainer.innerHTML = ""; // Limpiar contenido previo
+            formContainer.innerHTML = "";
             formContainer.appendChild(finTitle);
         } else {
             let finalOptions = [];
@@ -74,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } else if (currentSrc.includes("h2_d2.mp4")) {
                 finalOptions = [
                     { video: "h2_d2_sec1_final.mp4", title: "Ir al cuarto 3" },
-                    { video: "h2_d2_sec_final.mp4", title: "Ir al Rooftop" },
+                    { video: "h2_d2_sec2_final.mp4", title: "Ir al Rooftop" },
                 ];
             }
 
@@ -82,18 +96,25 @@ document.addEventListener("DOMContentLoaded", function () {
             startProgressBar();
 
             const finalForm = createForm(finalOptions);
-            formContainer.innerHTML = ""; // Limpiar contenido previo
+            formContainer.innerHTML = "";
             formContainer.appendChild(finalForm);
         }
     }
 
     function hideForm() {
         formContainer.innerHTML = "";
+        isDecisionPrompted = false;
     }
 
     function createForm(options) {
         const form = document.createElement("form");
 
+        // Agregar la barra de progreso al formulario
+        appendProgressBar(form);
+    
+        // Crear contenedor para los botones
+        const buttonsContainer = document.createElement("div");
+        
         options.forEach((option) => {
             const button = document.createElement("button");
             button.type = "button";
@@ -103,20 +124,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 hideForm();
                 startStory(option.video);
             });
-            form.appendChild(button);
+    
+            // Agregar botón al contenedor de botones
+            buttonsContainer.appendChild(button);
         });
-
-        // Agregar la barra de progreso al formulario
-        appendProgressBar(form);
-
+    
+        // Agregar el contenedor de botones al formulario
+        form.appendChild(buttonsContainer);
+        
         return form;
     }
+    
 
     function startStory(videoName) {
-        videoPlayer.src = "videos/home/" + videoName;
-        videoPlayer.play();
-        resetProgressBar();
-        startProgressBar(videoPlayer.src);
+        if (videoPlayer.src !== "videos/home/" + videoName) {
+            videoPlayer.src = "videos/home/" + videoName;
+            videoPlayer.play();
+            resetProgressBar();
+            startProgressBar(videoPlayer.src);
+            isVideoPlaying = true;
+            playButton.style.display = "none"; // Ocultar el botón de reproducción cuando se inicie un nuevo vídeo
+        }
     }
 
     function appendProgressBar(container) {
@@ -128,7 +156,7 @@ document.addEventListener("DOMContentLoaded", function () {
         container.appendChild(progressBar);
     }
 
-    function startProgressBar(currentSrc) {
+    function startProgressBar() {
         let progressValue = 100;
         const interval = (progressBarDuration * 1000) / 100;
 
@@ -144,10 +172,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (progressValue <= 0) {
                     clearInterval(progressInterval);
                     resetProgressBar();
-
-                    // Si el usuario no ha tomado una decisión, cargar un clip aleatorio
-                    if (!currentSrc.includes("_final")) {
-                        loadRandomClip(currentSrc);
+                    if (!isVideoPlaying) {
+                        loadRandomClip();
                     }
                 }
             }, interval);
@@ -162,23 +188,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function hideProgressAndFormContainer() {
-        // Ocultar el contenedor que alberga la barra de progreso y el formulario
         progressAndFormContainer.style.display = "none";
     }
 
-    function loadRandomClip(currentSrc) {
-        // Ocultar el formulario y la barra de progreso
+    function loadRandomClip() {
         hideForm();
         hideProgressAndFormContainer();
-    
-        // Lógica para cargar un clip aleatorio entre las opciones predeterminadas
+
         let defaultOptions;
-        if (currentSrc.includes("h2_d2.mp4")) {
+        if (videoPlayer.src.includes("h2_d2.mp4")) {
             defaultOptions = [
                 { video: "h2_d2_sec1_final.mp4", title: "Ir al cuarto 3" },
-                { video: "h2_d2_sec_final.mp4", title: "Ir al Rooftop" },
+                { video: "h2_d2_sec2_final.mp4", title: "Ir al Rooftop" },
             ];
-        } else if (currentSrc.includes("h2.mp4")) {
+        } else if (videoPlayer.src.includes("h2.mp4")) {
             defaultOptions = [
                 { video: "h2_d1_final.mp4", title: "Ponerse a trabajar" },
                 { video: "h2_d2.mp4", title: "Subir al 3er piso" },
@@ -189,7 +212,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 { video: "h2.mp4", title: "Sentarse en la silla" },
             ];
         }
-    
+
         const randomOption = defaultOptions[Math.floor(Math.random() * defaultOptions.length)];
         startStory(randomOption.video);
     }
